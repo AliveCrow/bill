6f525edbbbdc545317037759312569af39b988b3
grit <136907108@qq.com>
2020-11-03 21:38:34 +0800

    NumPad底部问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,22 +1,22 @@
 <template>
-    <div id="money" style="padding-bottom: 100px">
-      <Layout className="layout_one" ref="layout_one" key="101010"  >
-        <HeaderMain  key="1001" :allYearPersent.sync="allYearPersent" :billyType.sync="billyType"/>
-        <div key="1002" class="billy_box">
-          <transition name="list">
-            <div class="info_box" key="911" v-if="billyType === '月账单'">
-              <span class="title">月账单</span>
-              <transition name="toggle_ani">
-                <InfoList :key="uns2"/>
-              </transition>
-            </div>
-            <div v-else class="year_billy">
-              <span class="title">年统计</span>
-              <ul>
-                <li>
+  <div id="money">
+    <Layout className="layout_one" ref="layout_one" key="101010">
+      <HeaderMain key="1001" :allYearPersent.sync="allYearPersent" :billyType.sync="billyType"/>
+      <div key="1002" class="billy_box">
+        <transition name="list">
+          <div class="info_box" key="911" v-if="billyType === '月账单'" style="padding-bottom: 100px;">
+            <span class="title">月账单</span>
+            <transition name="toggle_ani">
+              <InfoList :key="uns2"/>
+            </transition>
+          </div>
+          <div v-else class="year_billy">
+            <span class="title">年统计</span>
+            <ul>
+              <li>
               <span class="year_pay">
                 <span
-                    style="font-size:.7rem">年支出: {{allYearPersent.pay}}</span>
+                    style="font-size:.7rem">年支出: {{ allYearPersent.pay }}</span>
                 <div :style="
                 'background: rgb(255,72,72);background: linear-gradient(90deg, rgba(255,72,72,1) 0%, rgba(255,72,72,1) '
                 +`${allYearPersent.pay/allYearPersent.allYearTotal*100+'%'}`+ ',rgba(240,240,240,1) '
@@ -25,11 +25,11 @@
                 >
                 </div>
               </span>
-                </li>
-                <li>
+              </li>
+              <li>
               <span class="year_in">
                 <span
-                    style="font-size:.7rem">年收入: {{allYearPersent.InCome}}</span>
+                    style="font-size:.7rem">年收入: {{ allYearPersent.InCome }}</span>
               <div :style="
               'background: rgb(103,194,58);background: linear-gradient(90deg, rgba(103,194,58,1) 0%, rgba(103,194,58,1) '
               +`${allYearPersent.InCome/allYearPersent.allYearTotal*100+'%'}`+ ',rgba(240,240,240,1) '
@@ -38,41 +38,41 @@
               >
               </div>
               </span>
-                </li>
-              </ul>
-            </div>
-          </transition>
-
-        </div>
-        <eva-icon key="1003" name="edit-outline" fill="#3da75b" class="icons" animation="shake" height="100%"
-                  @click="showNumPad"></eva-icon>
-      </Layout >
-      <div class="money_box go go_bottom" ref="NumPad" :style="{height:h+'px'}" >
-        <div class="money_box_con">
-          <eva-icon name="close-outline" fill="#3da75b" class="back" animation="shake" height="100%"
-                    @click="hideNumPad"></eva-icon>
-          <NumberPad :value.sync="recordListItem.num" @submit="submit" :key="uns1" style="padding-bottom: 160px" />
-          <Types @exposeType="getType" :value="recordListItem.types"/>
-          <div style="height: 53px;position: relative;">
-            <label for="start" class="header_input">
-              <input type="date" id="start" name="start"
-                     min="2020-01-01" ref="year_day"
-                     @change="setCreateDate"
-              >
-            </label>
+              </li>
+            </ul>
           </div>
-          <label class="remark" >
-            <span>备注:</span>
-            <input v-model="recordListItem.remark" type="text" placeholder="请在这里输入备注">
-          </label>
-          <Tag  :selectedTags.sync="recordListItem.tags" :key="uns3" :isError.sync="isError"/>
-          <Message :type="msg_type" ref="msg" v-show="msg_show">
-            {{ msg }}
-          </Message>
-        </div>
+        </transition>
 
       </div>
+      <eva-icon key="1003" name="edit-outline" fill="#3da75b" class="icons" animation="shake" height="100%"
+                @click="showNumPad"></eva-icon>
+    </Layout>
+    <div class="money_box go go_bottom" ref="NumPad" :style="{height:h+'px'}">
+      <div class="money_box_con">
+        <eva-icon name="close-outline" fill="#3da75b" class="back" animation="shake" height="100%"
+                  @click="hideNumPad"></eva-icon>
+        <NumberPad :value.sync="recordListItem.num" @submit="submit" :key="uns1" :style="{'padding-bottom': ph+'px'}"/>
+        <Types @exposeType="getType" :value="recordListItem.types"/>
+        <div style="height: 53px;position: relative;">
+          <label for="start" class="header_input">
+            <input type="date" id="start" name="start"
+                   min="2020-01-01" ref="year_day"
+                   @change="setCreateDate"
+            >
+          </label>
+        </div>
+        <label class="remark">
+          <span>备注:</span>
+          <input v-model="recordListItem.remark" type="text" placeholder="请在这里输入备注">
+        </label>
+        <Tag :selectedTags.sync="recordListItem.tags" :key="uns3" :isError.sync="isError"/>
+        <Message :type="msg_type" ref="msg" v-show="msg_show">
+          {{ msg }}
+        </Message>
+      </div>
+
     </div>
+  </div>
 </template>
 <script lang='ts'>
 import Vue from 'vue';
@@ -100,21 +100,28 @@ export default class Money extends mixins(listDepository) {
   list: string[] = [];
   uns1: number = 1;
   uns2: number = 2;
-  uns3: number = 3;
+  uns3: number = 3
   isError: boolean = false;
   msg_show: boolean = false;
   allYearPersent = {};
   billyType: string = '月账单';
-  h:number =0;
+  h: number = 0;
+  ph: number = 63;
 
-  created(){
+  created() {
     this.h = document.body.clientHeight;
   }
 
   mounted() {
     // @ts-ignore
     this.$refs.year_day.value = dayjs().format('YYYY-MM-DD');
+    window.onresize = () => {
+      return (() => {
+        this.ph = 180;
+      })();
+    };
   }
+
 
   getTags(list: string[]) {
     this.recordListItem.tags = list;
@@ -151,7 +158,7 @@ export default class Money extends mixins(listDepository) {
     setTimeout(() => {
       this.resetNumPad();
       // @ts-ignore
-      this.el.style.display ='none'
+      this.el.style.display = 'none';
     }, 500);
   }
 
@@ -161,13 +168,13 @@ export default class Money extends mixins(listDepository) {
     this.uns3 = 3;
     this.el = this.$refs.NumPad as Element;
     // @ts-ignore
-    this.el.style.display ='flex'
-    setTimeout(()=>{
+    this.el.style.display = 'flex';
+    setTimeout(() => {
       // @ts-ignore
       this.el.classList.remove('go_bottom');
       // @ts-ignore
       this.el.classList.add('go_top');
-    },100)
+    }, 100);
   }
 
   submit() {
@@ -196,7 +203,7 @@ export default class Money extends mixins(listDepository) {
 
   @Watch('isError')
   onch() {
-    console.log(this.isError );
+    console.log(this.isError);
     if (this.isError) {
       this.showMsg('标签创建失败,请检查是否有重名标签并且不能为空', 'Danger');
       setTimeout(() => {
@@ -215,6 +222,7 @@ export default class Money extends mixins(listDepository) {
 .toggle_ani-leave-active {
   transition: opacity .3s;
 }
+
 .toggle_ani-enter,
 .toggle_ani-leave-to {
   opacity: 0;
@@ -223,27 +231,29 @@ export default class Money extends mixins(listDepository) {
 //.list
 .list-enter-active,
 .list-leave-active {
-  animation: list_ani 0.4s cubic-bezier(.17,.67,.02,.97);
+  animation: list_ani 0.4s cubic-bezier(.17, .67, .02, .97);
 }
-.list-enter,.list-leave-to{
+
+.list-enter, .list-leave-to {
   transform: translateX(0);
   display: none;
 }
 
 
 @keyframes list_ani {
-  0%{
+  0% {
     transform: translateX(50%);
   }
-  100%{
+  100% {
     transform: translateX(0);
   }
 }
 
-#app{
+#app {
   overflow: hidden;
   height: 100vh;
 }
+
 .icons {
   background-color: rgba($whiteColor, 1);
   box-shadow: 0 0 10px rgba(#000, .2);
@@ -289,6 +299,7 @@ export default class Money extends mixins(listDepository) {
 
       .year_in {
         color: $Success;
+
         > div {
           border-radius: 10px;
           margin-bottom: 10px;
@@ -325,13 +336,12 @@ export default class Money extends mixins(listDepository) {
   display: none;
   flex-direction: column-reverse;
   width: 100%;
-  //height: 100vh;
   position: absolute;
   top: 0;
   background-color: #fff;
-  padding-bottom: 63px;
   overflow: hidden;
-  .money_box_con{
+
+  .money_box_con {
     display: flex;
     flex-direction: column-reverse;
     position: relative;
@@ -339,6 +349,7 @@ export default class Money extends mixins(listDepository) {
     width: 100%;
     overflow: auto;
   }
+
   .back {
     position: fixed;
     top: 20px;
@@ -376,9 +387,11 @@ export default class Money extends mixins(listDepository) {
     display: flex;
     align-items: center;
     min-height: 3rem;
+
     span {
       width: 4rem;
     }
+
     input {
       padding: 1px 8px;
       width: 100%;
